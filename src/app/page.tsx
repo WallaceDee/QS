1845825f823bd7c25da29d93d1db367c4fa3901e
chenguohua <lg447363121@163.com>
2026-02-28 13:28:07 +0800

    no message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { TeamSection } from "@/components/team-section"
 import { CTASection } from "@/components/cta-section"
 
 export default function Home() {
+  console.log("更新时间：2026-02-28 10:00:00")
   return (
     <div className="flex flex-col">
       <HeroSection />
